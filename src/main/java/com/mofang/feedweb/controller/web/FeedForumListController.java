9c55e94db1d3ce8a4f3662d804bd91c4484d7a9c
kehuazi <kehuazi@mofang.com>
2015-06-04 10:52:39 +0800

    up

--- src/main/java/com/mofang/feedweb/controller/web/FeedForumListController.java
+++ src/main/java/com/mofang/feedweb/controller/web/FeedForumListController.java
@@ -1,77 +1,127 @@
-//package com.mofang.feedweb.controller.web;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.mofang.feedweb.entity.FeedForum;
-//import com.mofang.feedweb.global.Constant;
-//import com.mofang.feedweb.service.impl.FeedForumListServiceImpl;
-//import com.mofang.feedweb.util.Tools;
-//
-//public class FeedForumListController {
-//
-//
-//	private FeedForumListServiceImpl forumlistService = new FeedForumListServiceImpl();
-//	private StringBuffer message = null;
-//	
-//	//版块列表
-//	//type:热门游戏  ：1  新游推荐 ：2
-//	@RequestMapping(value = "/ForumList",method = RequestMethod.GET)
-//	@ResponseBody
-//	public String initsearchPage(@RequestBody String letterGroup, int p, int type,HttpServletRequest request) throws Exception {
-//		message = new StringBuffer();
-//		
-//		//获取版块list
-//		JSONObject forumListResult = forumlistService.getForumListByLetterGroup(p, Constant.PAGE_SIZE, type, letterGroup);
-//		if (null != forumListResult) {
-//			int code = forumListResult.optInt("code", -1);
-//			if (0 != code) {
-//				message.append(forumListResult.optString("message", ""));
-//			} else {
-//				JSONObject data = forumListResult.optJSONObject("data");
-//				
-//				//通过总数算出页码
-//				int total = data.optInt("total",0);
-//				request.setAttribute("pageList", Tools.editPageNumber(total, p));
-//				request.setAttribute("page", p);
-//				request.setAttribute("totalPages", total);
-//				
-//				JSONArray list = data.optJSONArray("list");
-//				List<FeedForum> listForum = new ArrayList<FeedForum>();
-//				FeedForum objFeedForum = null;
-//				JSONObject jsonFeedForum = null;
-//				for(int i=0; i<list.length(); i++)
-//				{
-//					jsonFeedForum = list.getJSONObject(i);
-//					objFeedForum = new FeedForum();
-//					objFeedForum.setForum_id(jsonFeedForum.optLong("fid", 0));
-//					objFeedForum.setForum_name(jsonFeedForum.optString("name", ""));
-//					objFeedForum.setTotal_threads(jsonFeedForum.optInt("threads", 0));
-//					objFeedForum.setToday_threads(jsonFeedForum.optInt("today_threads", 0));
-//					objFeedForum.setForum_url("");
-//					objFeedForum.setGift_url(jsonFeedForum.optString("gift_url", ""));
-//					objFeedForum.setPrefecture_url(jsonFeedForum.optString("prefecture_url", ""));
-//					listForum.add(objFeedForum);
-//				}
-//				
-//				request.setAttribute("forumList", listForum);
-//				
-//			}	
-//		} else {
-//			request.setAttribute("forumList", new ArrayList<FeedForum>());
-//		}
-//		
-//		
-//		return "forumList";
-//
-//	}
-//}
+package com.mofang.feedweb.controller.web;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mofang.feedweb.entity.ForumListInfo;
+import com.mofang.feedweb.form.FeedForumListForm;
+import com.mofang.feedweb.global.Constant;
+import com.mofang.feedweb.util.StringUtil;
+import com.mofang.feedweb.util.Tools;
+
+public class FeedForumListController extends FeedCommonController {
+	
+	
+	//版块列表
+	@RequestMapping(value = "/forumList")
+	@ResponseBody
+	public ModelAndView forumList(HttpServletRequest request) throws Exception {
+		
+		String letterGroup = "";
+		if (!StringUtil.isNullOrEmpty(request.getParameter("letterGroup"))) {
+			letterGroup = request.getParameter("letterGroup");
+		}
+		int currePage = 1;
+		if (!StringUtil.isNullOrEmpty(request.getParameter("currePage"))) {
+			currePage = Integer.valueOf(
+					Tools.replaceBlank(request.getParameter("currePage")));
+		}
+		int forumType = 1;//热门游戏  ：1  新游推荐 ：2
+		if (!StringUtil.isNullOrEmpty(request.getParameter("forumType"))) {
+			forumType = Integer.valueOf(
+					Tools.replaceBlank(request.getParameter("forumType")));
+		}
+		
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		//获取版块list
+		FeedForumListForm form = getForumList(request, currePage, forumType, letterGroup);
+		int total = form.getTotal();
+		
+		
+		model.put("listInfo", form.getInfoList());
+		model.put("searchkey", "");
+		model.put("currePage", currePage);
+		model.put("totalPages", Tools.editTotalPageNumber(total));
+		model.put("pagelist", Tools.editPageNumber(total, currePage));
+		model.put("forumType", forumType);
+		
+		return new ModelAndView("forumList", model);
+
+	}
+	
+	private FeedForumListForm getForumList(
+			HttpServletRequest request, int p, int type, String letterGroup) throws JSONException 
+	{
+		
+		FeedForumListForm form = new FeedForumListForm();
+		
+		StringBuffer name = new StringBuffer();
+		StringBuffer content = new StringBuffer();
+		name.append("p");
+		name.append(Constant.STR_COMMA);
+		name.append("type");
+		name.append(Constant.STR_COMMA);
+		name.append("pagesize");
+		name.append(Constant.STR_COMMA);
+		name.append("letterGroup");
+		content.append(p);
+		content.append(Constant.STR_COMMA);
+		content.append(type);
+		content.append(Constant.STR_COMMA);
+		content.append(Constant.PAGE_SIZE);
+		content.append(Constant.STR_COMMA);
+		content.append(letterGroup);
+		
+		String parameterURL = Tools.editURLParameter(name.toString(),content.toString());
+		JSONObject result = getHttpInfo(getFeedUrlInfo().concat(Constant.LIST_FORUM_GET_URL),
+				parameterURL, request);
+		List<ForumListInfo> listInfo = new ArrayList<ForumListInfo>();
+		int total = 0;
+		if (null != result) {
+			int code = result.optInt("code", -1);
+			if (0 != code) {
+			} else {
+				
+				JSONObject data = result.optJSONObject("data");
+				total = data.optInt("total", 0);
+				JSONArray list = data.optJSONArray("list");
+				JSONObject json = null;
+				ForumListInfo info = null;
+				for(int i=0; i<list.length(); i++)
+				{
+					info = new ForumListInfo();
+					json = list.getJSONObject(i);
+					info.setForumId(json.optLong("fid", 0));
+					info.setGameId(json.optLong("game_id", 0));
+					info.setForumName(json.optString("name", ""));
+					info.setIcon(json.optString("icon", ""));
+					info.setTodayThreads(json.optInt("today_threads", 0));
+					info.setTotalThreads(json.optInt("total_threads", 0));
+					info.setPrefectureUrl(json.optString("prefecture_url", ""));
+					info.setGiftUrl(json.optString("gift_url", ""));
+					info.setDownLoadUrl(json.optString("download_url", ""));
+					listInfo.add(info);
+				}
+			}	
+			
+		}
+		
+		form.setInfoList(listInfo);
+		form.setTotal(total);
+		
+		return form;
+		
+	}
+}
