563136ca50565787456913f01d209039fc0d5cc0
kehuazi <kehuazi@mofang.com>
2015-06-06 18:14:16 +0800

    up

--- src/main/java/com/mofang/feedweb/controller/web/FeedForumListController.java
+++ src/main/java/com/mofang/feedweb/controller/web/FeedForumListController.java
@@ -27,7 +27,7 @@ public class FeedForumListController extends FeedCommonController {
 	//版块列表
 	@RequestMapping(value = "/forumList")
 	public ModelAndView forumList(HttpServletRequest request) throws Exception {
-		
+		 
 		int letterGroup = 1;
 		if (!StringUtil.isNullOrEmpty(request.getParameter("letterGroup"))) {
 			letterGroup = Integer.valueOf(request.getParameter("letterGroup"));
