94d011647869d42643ece7e3b1ab71370efd89d4
kehuazi <kehuazi@mofang.com>
2015-06-16 18:03:45 +0800

    #update try catch 追加

--- src/main/java/com/mofang/feedweb/controller/FeedForumContentController.java
+++ src/main/java/com/mofang/feedweb/controller/FeedForumContentController.java
@@ -31,6 +31,7 @@ import com.mofang.feedweb.entity.RoleInfo;
 import com.mofang.feedweb.entity.UserInfo;
 import com.mofang.feedweb.global.Constant;
 import com.mofang.feedweb.global.ForumType;
+import com.mofang.feedweb.global.GlobalObject;
 import com.mofang.feedweb.util.SignUtil;
 import com.mofang.feedweb.util.StringUtil;
 import com.mofang.feedweb.util.Tools;
@@ -48,56 +49,63 @@ public class FeedForumContentController extends FeedCommonController {
 	public ModelAndView forumContent(HttpServletRequest request, @RequestParam("fid") long fid) throws Exception {
 		Map<String, Object> model = new HashMap<String, Object>();
 		
-		// 获取版块信息
-		FeedForum feedForum = getFeedForumInfo(request, fid);
-		
-		// 获取该版块的吧主列表
-		List<RoleInfo> roleList= getFeedForumRoleInfoList(request, fid, model);
-		feedForum.setRoleList(roleList);
-		
-		// 获取版块下的帖子列表
-		List<FeedThread> threadList = getThreadList(request, fid, model);
-//		model.put("threadList", threadList);
-		
-		// if 是官方版块，获取头条列表和新游推荐
-		int type = feedForum.getType();
-		if (type == ForumType.OFFICIAL) {
-			//头条列表
-			List<HotThread> hotThreadList = getHotThreads(request);
-			model.put("hotThreadList", hotThreadList);
+		try {
+			// 获取版块信息
+			FeedForum feedForum = getFeedForumInfo(request, fid);
 			
-			// 新游推荐
-			List<NewGame> newGameList = getNewGameList(request);
-			model.put("newGameList", newGameList);
+			// 获取该版块的吧主列表
+			List<RoleInfo> roleList= getFeedForumRoleInfoList(request, fid, model);
+			feedForum.setRoleList(roleList);
 			
-		} else { // else 获取该版块关联的游戏信息和礼包发号
-			// 游戏信息
-			int gameId = feedForum.getGameId();
-			Game game = getGameInfo(request, gameId);
+			// 获取版块下的帖子列表
+			List<FeedThread> threadList = getThreadList(request, fid, model);
+	//		model.put("threadList", threadList);
 			
-			model.put("game", game);
+			// if 是官方版块，获取头条列表和新游推荐
+			int type = feedForum.getType();
+			if (type == ForumType.OFFICIAL) {
+				//头条列表
+				List<HotThread> hotThreadList = getHotThreads(request);
+				model.put("hotThreadList", hotThreadList);
+				
+				// 新游推荐
+				List<NewGame> newGameList = getNewGameList(request);
+				model.put("newGameList", newGameList);
+				
+			} else { // else 获取该版块关联的游戏信息和礼包发号
+				// 游戏信息
+				int gameId = feedForum.getGameId();
+				Game game = getGameInfo(request, gameId);
+				
+				model.put("game", game);
+				
+				// 礼包发号
+				List<GameGift> giftList = getGiftList(request, gameId);
+				model.put("giftList", giftList);
+			}
+			model.put("keyword", getSearchKey(request));
+			model.put("feedForum", feedForum);
 			
-			// 礼包发号
-			List<GameGift> giftList = getGiftList(request, gameId);
-			model.put("giftList", giftList);
-		}
-		model.put("keyword", getSearchKey(request));
-		model.put("feedForum", feedForum);
-		
-		// 关注状态
-		UserInfo userInfo = getUserInfo(request);
-		if(userInfo == null) {
-			model.put("isFollow", false);
-		}else {
-			boolean isFollow = isFollow(userInfo.getUserId(), fid, request);
-			model.put("isFollow", isFollow);
-		}
-		
-		return new ModelAndView("forum_content", model);
+			// 关注状态
+			UserInfo userInfo = getUserInfo(request);
+			if(userInfo == null) {
+				model.put("isFollow", false);
+			}else {
+				boolean isFollow = isFollow(userInfo.getUserId(), fid, request);
+				model.put("isFollow", isFollow);
+			}
+			
+			return new ModelAndView("forum_content", model);
+		} catch (Exception e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.forumContent throw an error.", e);
+			return new ModelAndView("forum_content", model);
+		} 
 	}
 	
 
 	private boolean isFollow(long userId, long forumId, HttpServletRequest request) {
+		
 		JSONObject json = getHttpInfo(getUserInfoUrl() + Constant.USER_IS_FOLLOW_URL, "?uid=" + userId + "&area_id=" + forumId, request);
 		if(json == null) {
 			return false;
@@ -117,21 +125,27 @@ public class FeedForumContentController extends FeedCommonController {
 	public String follow(@RequestParam("fid") long forumId, @RequestParam("is_follow") int isFollow, 
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		JSONObject postData = new JSONObject();
-		postData.put("area_id", forumId);
-		postData.put("dofollow", isFollow);
-		
-		
-		JSONObject json = postHttpInfo(getForumFollowUrl(), postData, request);
-		
-		response.setContentType("text/html; charset=UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter();
-		out.print(json);
-		out.flush();
-		out.close();
-		
-		return null;
+		try {
+			JSONObject postData = new JSONObject();
+			postData.put("area_id", forumId);
+			postData.put("dofollow", isFollow);
+			
+			
+			JSONObject json = postHttpInfo(getForumFollowUrl(), postData, request);
+			
+			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			PrintWriter out = response.getWriter();
+			out.print(json);
+			out.flush();
+			out.close();
+			
+			return null;
+		} catch (Exception e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.follow throw an error.", e);
+			return null;
+		}
 	}
 	
 	private List<GameGift> getGiftList(HttpServletRequest request, int gameId) {
@@ -190,142 +204,175 @@ public class FeedForumContentController extends FeedCommonController {
 
 	private List<NewGame> getNewGameList(HttpServletRequest request)
 			throws JSONException {
-		JSONObject json = getHttpInfo(getRecommendGameRankUrl(), "", request);
+		
 		List<NewGame> newGameList = new ArrayList<NewGame>();
 		
-		if (json != null && json.optInt("code", -1) == 0) {
-			JSONArray data = json.optJSONArray("data");
+		try {
 			
-			if (data != null && data.length() > 0) {
-				for (int i = 0; i < data.length(); i++) {
-					JSONObject obj = data.getJSONObject(i);
-					NewGame newGame = new NewGame();
-					newGame.setForumId(obj.optLong("forum_id", 0));
-					newGame.setForumName(obj.optString("forum_name", ""));
-					newGame.setIcon(obj.optString("icon", ""));
-					newGame.setDownloadUrl(obj.optString("download_url"));
-					newGame.setGiftUrl(obj.optString("gift_url", ""));
-					
-					newGameList.add(newGame);
+			JSONObject json = getHttpInfo(getRecommendGameRankUrl(), "", request);
+			
+			if (json != null && json.optInt("code", -1) == 0) {
+				JSONArray data = json.optJSONArray("data");
+				
+				if (data != null && data.length() > 0) {
+					for (int i = 0; i < data.length(); i++) {
+						JSONObject obj = data.getJSONObject(i);
+						NewGame newGame = new NewGame();
+						newGame.setForumId(obj.optLong("forum_id", 0));
+						newGame.setForumName(obj.optString("forum_name", ""));
+						newGame.setIcon(obj.optString("icon", ""));
+						newGame.setDownloadUrl(obj.optString("download_url"));
+						newGame.setGiftUrl(obj.optString("gift_url", ""));
+						
+						newGameList.add(newGame);
+					}
 				}
 			}
+			return newGameList;
+		} catch (JSONException e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.getNewGameList throw an error.", e);
+			return newGameList;
 		}
-		return newGameList;
 	}
 
 	private List<HotThread> getHotThreads(HttpServletRequest request)
 			throws JSONException {
-		JSONObject json = getHttpInfo(getTopThreadsUrl(), "", request);
 		List<HotThread> hotThreadList = new ArrayList<HotThread>();
 		
-		if (json != null && json.optInt("code", -1) == 0) {
-			JSONArray data = json.optJSONArray("data");
+		try {
+			JSONObject json = getHttpInfo(getTopThreadsUrl(), "", request);
 			
-			if (data != null && data.length() > 0) {
-				for (int i = 0; i < data.length(); i++) {
-					JSONObject obj = data.getJSONObject(i);
-					HotThread hotThread = new HotThread(obj.optLong("thread_id", 0), obj.optString("subject", ""));
-					hotThreadList.add(hotThread);
+			if (json != null && json.optInt("code", -1) == 0) {
+				JSONArray data = json.optJSONArray("data");
+				
+				if (data != null && data.length() > 0) {
+					for (int i = 0; i < data.length(); i++) {
+						JSONObject obj = data.getJSONObject(i);
+						HotThread hotThread = new HotThread(obj.optLong("thread_id", 0), obj.optString("subject", ""));
+						hotThreadList.add(hotThread);
+					}
 				}
 			}
+			return hotThreadList;
+		} catch (JSONException e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.getHotThreads throw an error.", e);
+			return hotThreadList;
 		}
-		return hotThreadList;
 	}
 	
-	private List<RoleInfo> getFeedForumRoleInfoList(HttpServletRequest request, long fid, Map<String, Object> model) throws Exception {
-		String param = "fid=" + fid;
-		boolean isFull = false;
-		JSONObject json = getHttpInfo(getRoleInfoListGetUrl(), param, request);
+	private List<RoleInfo> getFeedForumRoleInfoList(HttpServletRequest request,
+			long fid, Map<String, Object> model) throws Exception {
+		
 		List<RoleInfo> roleList = new ArrayList<RoleInfo>();
-		if (json != null && json.optInt("code", -1) == 0) {
+		try {
+			String param = "fid=" + fid;
+			boolean isFull = false;
+			JSONObject json = getHttpInfo(getRoleInfoListGetUrl(), param, request);
 			
-			JSONObject data = json.optJSONObject("data");
-			if (data != null) {
-				isFull = data.optBoolean("is_full", false);
-				JSONArray moderators = data.optJSONArray("moderators");
-				for (int i = 0; i < moderators.length(); i++) {
-					JSONObject obj = moderators.getJSONObject(i);
-					RoleInfo roleInfo = new RoleInfo(obj.optLong("user_id", 0), obj.optString("nickname", ""), obj.optString("avatar", ""));
-					roleList.add(roleInfo);
+			if (json != null && json.optInt("code", -1) == 0) {
+				
+				JSONObject data = json.optJSONObject("data");
+				if (data != null) {
+					isFull = data.optBoolean("is_full", false);
+					JSONArray moderators = data.optJSONArray("moderators");
+					for (int i = 0; i < moderators.length(); i++) {
+						JSONObject obj = moderators.getJSONObject(i);
+						RoleInfo roleInfo = new RoleInfo(obj.optLong("user_id", 0), obj.optString("nickname", ""), obj.optString("avatar", ""));
+						roleList.add(roleInfo);
+					}
 				}
 			}
+			model.put("is_full", isFull);
+			return roleList;
+		} catch (Exception e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.getFeedForumRoleInfoList throw an error.", e);
+			return roleList;
 		}
-		model.put("is_full", isFull);
-		return roleList;
 	}
 	
 	private List<FeedThread> getThreadList(HttpServletRequest request, long fid, Map<String, Object> model) throws Exception {
+		
 		List<FeedThread> threadList = new ArrayList<FeedThread>();
-		int total = 0;
-		int threadType = 0; // 0表示普通，1表示精华
-		String typeString = request.getParameter("type");
-		if (typeString != null && StringUtil.isInteger(typeString)) {
-			threadType = Integer.parseInt(typeString);
-		}
 		
-		int timeType = 0; // 0表示按回复时间排序、1表示按发帖时间排序
-		
-		String timeTypeString = request.getParameter("timeType");
-		if (timeTypeString != null && StringUtil.isInteger(timeTypeString)) {
-			timeType = Integer.parseInt(timeTypeString);
-		}
-		
-		int p = 1;
-		String pString = request.getParameter("currentPage");
-		if (pString != null && StringUtil.isInteger(pString)) {
-			p = Integer.parseInt(pString);
-		}
-		
-		int pageSize = 20;
-//		String pageSizeString = request.getParameter("pagesize");
-//		if (pageSizeString != null && StringUtil.isInteger(pageSizeString)) {
-//			pageSize = Integer.parseInt(pageSizeString);
-//		}
-		
-		if (p == 1) {
-			getThreadTopList(request, fid, model);
-		}
-		
-		StringBuilder paramBuilder = new StringBuilder();
-		paramBuilder.append("fid=").append(fid);
-		paramBuilder.append("&type=").append(threadType);
-		paramBuilder.append("&timetype=").append(timeType);
-		paramBuilder.append("&page=").append(p);
-		paramBuilder.append("&size=").append(pageSize);
-		
-		int tagId = 0;
-		String tagIdString = request.getParameter("tag_id");
-		if (tagIdString != null && StringUtil.isInteger(tagIdString)) {
-			tagId = Integer.parseInt(tagIdString);
-			paramBuilder.append("&tagid=").append(tagId);
-		}
-		
-		JSONObject json = getHttpInfo(getThreadListUrl(), paramBuilder.toString(), request);
-		
-		if (json != null && json.optInt("code", -1) == 0) {
-			JSONObject data = json.optJSONObject("data");
-			total = data.optInt("total", 0);
-			JSONArray threads = data.optJSONArray("threads");
-			if (threads != null && threads.length() > 0) {
-				for (int i = 0; i < threads.length(); i++) {
-					JSONObject obj = threads.getJSONObject(i);
-					FeedThread feedThread = this.fromJsonToFeedThread(obj);
-					
-					threadList.add(feedThread);
-				}
+		try {
+			int total = 0;
+			int threadType = 0; // 0表示普通，1表示精华
+			String typeString = request.getParameter("type");
+			if (typeString != null && StringUtil.isInteger(typeString)) {
+				threadType = Integer.parseInt(typeString);
 			}
 			
+			int timeType = 0; // 0表示按回复时间排序、1表示按发帖时间排序
+			
+			String timeTypeString = request.getParameter("timeType");
+			if (timeTypeString != null && StringUtil.isInteger(timeTypeString)) {
+				timeType = Integer.parseInt(timeTypeString);
+			}
+			
+			int p = 1;
+			String pString = request.getParameter("currentPage");
+			if (pString != null && StringUtil.isInteger(pString)) {
+				p = Integer.parseInt(pString);
+			}
+			
+			int pageSize = 20;
+	//		String pageSizeString = request.getParameter("pagesize");
+	//		if (pageSizeString != null && StringUtil.isInteger(pageSizeString)) {
+	//			pageSize = Integer.parseInt(pageSizeString);
+	//		}
+			
+			if (p == 1) {
+				getThreadTopList(request, fid, model);
+			}
+			
+			StringBuilder paramBuilder = new StringBuilder();
+			paramBuilder.append("fid=").append(fid);
+			paramBuilder.append("&type=").append(threadType);
+			paramBuilder.append("&timetype=").append(timeType);
+			paramBuilder.append("&page=").append(p);
+			paramBuilder.append("&size=").append(pageSize);
+			
+			int tagId = 0;
+			String tagIdString = request.getParameter("tag_id");
+			if (tagIdString != null && StringUtil.isInteger(tagIdString)) {
+				tagId = Integer.parseInt(tagIdString);
+				paramBuilder.append("&tagid=").append(tagId);
+			}
+			
+			JSONObject json = getHttpInfo(getThreadListUrl(), paramBuilder.toString(), request);
+			
+			if (json != null && json.optInt("code", -1) == 0) {
+				JSONObject data = json.optJSONObject("data");
+				total = data.optInt("total", 0);
+				JSONArray threads = data.optJSONArray("threads");
+				if (threads != null && threads.length() > 0) {
+					for (int i = 0; i < threads.length(); i++) {
+						JSONObject obj = threads.getJSONObject(i);
+						FeedThread feedThread = this.fromJsonToFeedThread(obj);
+						
+						threadList.add(feedThread);
+					}
+				}
+				
+			}
+			
+			model.put("threadList", threadList);
+			model.put("type", threadType);
+			model.put("timeType", timeType);
+			model.put("tag_id", tagId);
+			model.put("currentPage", p);
+			model.put("totalPages", Tools.editTotalPageNumber(total));
+			model.put("pagelist", Tools.editPageNumber(total, p,Constant.PAGE_SIZE));
+			
+			return threadList;
+		} catch (Exception e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.getThreadList throw an error.", e);
+			return threadList;
 		}
-		
-		model.put("threadList", threadList);
-		model.put("type", threadType);
-		model.put("timeType", timeType);
-		model.put("tag_id", tagId);
-		model.put("currentPage", p);
-		model.put("totalPages", Tools.editTotalPageNumber(total));
-		model.put("pagelist", Tools.editPageNumber(total, p,Constant.PAGE_SIZE));
-		
-		return threadList;
 	}
 	
 	private FeedThread fromJsonToFeedThread(JSONObject obj) {
@@ -379,24 +426,32 @@ public class FeedForumContentController extends FeedCommonController {
 	}
 	
 	private List<FeedThread> getThreadTopList(HttpServletRequest request, long forumId, Map<String, Object> model) throws Exception {
-		String param = "fid=" + forumId;
-		JSONObject json = getHttpInfo(getThreadTopListUrl(), param, request);
+		
 		List<FeedThread> topThreadList = new ArrayList<FeedThread>();
 		
-		if (json != null && json.optInt("code", -1) == 0) {
-			JSONObject data = json.optJSONObject("data");
-			JSONArray threads = data.optJSONArray("threads");
-			if (threads != null && threads.length() > 0) {
-				for (int i = 0; i < threads.length(); i++) {
-					JSONObject obj = threads.getJSONObject(i);
-					FeedThread feedThread = this.fromJsonToFeedThread(obj);
-					topThreadList.add(feedThread);
+		try {
+			String param = "fid=" + forumId;
+			JSONObject json = getHttpInfo(getThreadTopListUrl(), param, request);
+			
+			if (json != null && json.optInt("code", -1) == 0) {
+				JSONObject data = json.optJSONObject("data");
+				JSONArray threads = data.optJSONArray("threads");
+				if (threads != null && threads.length() > 0) {
+					for (int i = 0; i < threads.length(); i++) {
+						JSONObject obj = threads.getJSONObject(i);
+						FeedThread feedThread = this.fromJsonToFeedThread(obj);
+						topThreadList.add(feedThread);
+					}
 				}
 			}
+			
+			model.put("topThreadList", topThreadList);
+			return topThreadList;
+		} catch (Exception e) {
+			GlobalObject.ERROR_LOG.error(
+					"at FeedForumContentController.getThreadTopList throw an error.", e);
+			return topThreadList;
 		}
-		
-		model.put("topThreadList", topThreadList);
-		return topThreadList;
 		
 	}
 	
