07fb1f50f1624bce3803b7e440e4b0bf963d9866
kehuazi <kehuazi@mofang.com>
2015-07-23 14:57:11 +0800

    #伪静态

--- src/main/java/com/mofang/feedweb/global/CommonUrl.java
+++ src/main/java/com/mofang/feedweb/global/CommonUrl.java
@@ -17,14 +17,18 @@ public class CommonUrl {
 	private String mofang_home_url;
 	@Value("${bbs_home_url}")
 	private String bbs_home_url;
+	@Value("${base_url}")
+	private String base_url;
 	
 	public static String mofangHomeUrl;
 	public static String bbsHomeUrl;
+	public static String baseUrl;
 	
 	@PostConstruct
 	public void init() {
 		CommonUrl.mofangHomeUrl = mofang_home_url;
 		CommonUrl.bbsHomeUrl = bbs_home_url;
+		CommonUrl.baseUrl = base_url;
 	}
 	
 }
