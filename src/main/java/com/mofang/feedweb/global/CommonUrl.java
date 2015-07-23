a7c278433c17ae26a689531f98079b4549a57fe3
kehuazi <kehuazi@mofang.com>
2015-07-23 10:42:24 +0800

    #伪静态前版块恢复

--- src/main/java/com/mofang/feedweb/global/CommonUrl.java
+++ src/main/java/com/mofang/feedweb/global/CommonUrl.java
@@ -1,6 +1,7 @@
 package com.mofang.feedweb.global;
 
 import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,18 +17,14 @@ public class CommonUrl {
 	private String mofang_home_url;
 	@Value("${bbs_home_url}")
 	private String bbs_home_url;
-	@Value("${base_url}")
-	private String base_url;
 	
 	public static String mofangHomeUrl;
 	public static String bbsHomeUrl;
-	public static String baseUrl;
 	
 	@PostConstruct
 	public void init() {
 		CommonUrl.mofangHomeUrl = mofang_home_url;
 		CommonUrl.bbsHomeUrl = bbs_home_url;
-		CommonUrl.baseUrl = base_url;
 	}
 	
 }
